3b76b981e299bc14d5a09e0e7f882de18103275e
Utku Halis <20300056@emu.edu.tr>
2021-04-22 13:06:31 +0300

    Initial commit

---
+++ Basit Dosya İndirme/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Dim save As New SaveFileDialog
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If save.ShowDialog = Windows.Forms.DialogResult.OK Then
+            My.Computer.Network.DownloadFile(TextBox1.Text, save.FileName, False, False, True, "60", True)
+            TextBox2.Text = save.FileName
+        End If
+
+    End Sub
+End Class
